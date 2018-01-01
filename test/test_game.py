e8788c0f8834192e37625a3a06bd5268ab549097
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2018-01-01 19:28:04 +0000

    refactors tests to include roll_many method

--- test/test_game.py
+++ test/test_game.py
@@ -11,14 +11,16 @@ class GameTest(unittest.TestCase):
         self.assertEqual(self.game.calculate_score(), 0)
 
     def test_gutter_game_should_score_zero(self):
-        for i in range(0, 20):
-            self.game.roll(0)
+        self.roll_many(20, 0)
         self.assertEqual(self.game.calculate_score(), 0)
 
     def test_game_of_all_ones_should_score_twenty(self):
-        for i in range(0, 20):
-            self.game.roll(1)
+        self.roll_many(20, 1)
         self.assertEqual(self.game.calculate_score(), 20)
+
+    def roll_many(self, rolls, pins):
+        for i in range(0, rolls):
+            self.game.roll(pins)
 
 
 if __name__ == '__main__':
