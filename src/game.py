d9d3171fcc039052480018d67b2fc9a41eafce1a
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2018-01-01 19:20:19 +0000

    should initialise score to zero

--- src/game.py
+++ src/game.py
@@ -1,4 +1,10 @@
-class Game(object):
+class Game:
 
-    def score():
-        pass
+    def __init__(self):
+        self.score = 0
+
+    def calculate_score(self):
+        return self.score
+
+    def roll(self, pins):
+        self.score += pins
--- test/test_game.py
+++ test/test_game.py
@@ -7,6 +7,10 @@ class GameTest(unittest.TestCase):
     def test_should_create_game(self):
         game = Game()
 
+    def test_game_should_have_initial_score_zero(self):
+        game = Game()
+        self.assertEqual(game.calculate_score(), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
