1b14bb5a5dcbb0ec4db298370782ab5962a0506f
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2018-01-01 19:48:16 +0000

    refactors game to simplify code

--- src/game.py
+++ src/game.py
@@ -8,13 +8,22 @@ class Game:
         score = 0
         firstInFrame = 0
         for frame in range(0, 10):
-            if (self.rolls[firstInFrame] + self.rolls[firstInFrame+1] == 10):
-                score += 10 + self.rolls[firstInFrame+2]
+            if (self.__is_spare(firstInFrame)):
+                score += self.__calculate_spare(firstInFrame)
             else:
-                score += self.rolls[firstInFrame] + self.rolls[firstInFrame+1]
+                score += self.__calculate_frame(firstInFrame)
             firstInFrame += 2
         return score
 
     def roll(self, pins):
         self.rolls[self.currentRoll] = pins
         self.currentRoll += 1
+
+    def __is_spare(self, firstInFrame):
+        return self.rolls[firstInFrame] + self.rolls[firstInFrame+1] == 10
+
+    def __calculate_spare(self, firstInFrame):
+        return 10 + self.rolls[firstInFrame+2]
+
+    def __calculate_frame(self, firstInFrame):
+        return self.rolls[firstInFrame] + self.rolls[firstInFrame+1]
