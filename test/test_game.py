ddb94af281e1728ed14d834554529db6901cfd3b
Michael Ruocco <michael.ruocco@travisperkins.co.uk>
2018-01-01 19:25:52 +0000

    refactors tests

--- test/test_game.py
+++ test/test_game.py
@@ -4,12 +4,21 @@ from src.game import Game
 
 class GameTest(unittest.TestCase):
 
-    def test_should_create_game(self):
-        game = Game()
+    def setUp(self):
+        self.game = Game()
 
     def test_game_should_have_initial_score_zero(self):
-        game = Game()
-        self.assertEqual(game.calculate_score(), 0)
+        self.assertEqual(self.game.calculate_score(), 0)
+
+    def test_gutter_game_should_score_zero(self):
+        for i in range(0, 20):
+            self.game.roll(0)
+        self.assertEqual(self.game.calculate_score(), 0)
+
+    def test_game_of_all_ones_should_score_twenty(self):
+        for i in range(0, 20):
+            self.game.roll(1)
+        self.assertEqual(self.game.calculate_score(), 20)
 
 
 if __name__ == '__main__':
